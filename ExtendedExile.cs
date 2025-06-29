7b81c77f84a1120af610870e6a04d636513a7037
Luan Emanuel da Silva <programar@luanemanuel.com.br>
2025-06-29 02:01:54 -0300

    feat(extended-exile): Bump version to 1.2.0 in plugin and manifest files

--- ExtendedExile.cs
+++ ExtendedExile.cs
@@ -6,7 +6,7 @@ using ExtendedExile.Utils;
 
 namespace ExtendedExile
 {
-    [BepInPlugin("br.com.luanemanuel.extendedexile", "Extended Exile", "1.0.0")]
+    [BepInPlugin("br.com.luanemanuel.extendedexile", "Extended Exile", "1.2.0")]
     public class ExtendedExilePlugin : BaseUnityPlugin
     {
         internal new static Config Config { get; private set; }
